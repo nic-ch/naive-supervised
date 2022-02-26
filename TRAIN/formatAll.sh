72408cc1c79f7e89d1ced3bc41a2f57d061ff1c0
Nicolas Chaussé <nicolaschausse@protonmail.com>
2022-02-26 04:08:31 -0500

    Change clang-format from 13 to 14

--- TRAIN/formatAll.sh
+++ TRAIN/formatAll.sh
@@ -1,2 +1,2 @@
-clang-format-13 -i -style="{BasedOnStyle: Mozilla, IndentWidth: 2, ColumnLimit: 120}" *.cpp
-clang-format-13 -i -style="{BasedOnStyle: Mozilla, IndentWidth: 2, ColumnLimit: 120}" *.hpp
+clang-format-14 -i -style="{BasedOnStyle: Mozilla, IndentWidth: 2, ColumnLimit: 120}" *.cpp
+clang-format-14 -i -style="{BasedOnStyle: Mozilla, IndentWidth: 2, ColumnLimit: 120}" *.hpp
